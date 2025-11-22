39e8a84281c93ba99e9aa5b6be8c8d7f85839d03
Tanusree <tbyram@charlotte.edu>
2025-11-21 19:48:11 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className ="font-game text-2xl" >Welcome to the Home page</h1>
+    </div>
+  );
+}
